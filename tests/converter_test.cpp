e6e076abfd24621027ea995332c903a26cd1d366
Ian Cullen <ian.james.cullen@gmail.com>
2021-08-29 13:42:23 +0100

    Set up some initial benchmarking

--- tests/converter_test.cpp
+++ tests/converter_test.cpp
@@ -1,8 +1,8 @@
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
 #include "nuclear-data-reader/converter.hpp"
 
 #include <catch2/catch.hpp>
-
-#include <limits>
 
 
 TEST_CASE("String -> int", "[Converter]")
@@ -75,4 +75,52 @@ TEST_CASE("Float -> string", "[Converter]")
 {
   REQUIRE_THAT(Converter::FloatToNdp(1.234, 3), Catch::Matches("1.234"));
   REQUIRE_THAT(Converter::FloatToNdp(std::numeric_limits<double>::max(), 2), Catch::Matches("null"));
+}
+
+
+TEST_CASE("Generic string -> value", "[Converter]")
+{
+  std::string_view d_str{ "987.654" };
+  SECTION("Double") { REQUIRE(Converter::StringToNum<double>(d_str, 0, 7) == Approx(987.654)); }
+
+  std::string_view i8_str{ "123" };
+  SECTION("uint8_t") { REQUIRE(Converter::StringToNum<uint8_t>(i8_str, 0, 3) == 123); }
+
+  std::string_view i16_str{ "4563" };
+  SECTION("uint16_t") { REQUIRE(Converter::StringToNum<uint16_t>(i16_str, 0, 4) == 4563); }
+}
+
+
+TEST_CASE("", "[.Benchmark]")
+{
+  SECTION("Double")
+  {
+    BENCHMARK_ADVANCED("Double charconv")(Catch::Benchmark::Chronometer meter)
+    {
+      std::string_view sv_str{ "987.654" };
+      meter.measure([&sv_str]() { return Converter::StringToNum<double>(sv_str, 0, 7); });
+    };
+
+    BENCHMARK_ADVANCED("Double own")(Catch::Benchmark::Chronometer meter)
+    {
+      std::string str{ "987.654" };
+      meter.measure([&str]() { return Converter::StringToDouble(str, 0, 7); });
+    };
+  }
+
+  SECTION("Integer")
+  {
+    BENCHMARK_ADVANCED("Int charconv")(Catch::Benchmark::Chronometer meter)
+    {
+      std::string_view sv_str{ "123" };
+      meter.measure([&sv_str]() { return Converter::StringToNum<uint8_t>(sv_str, 0, 3); });
+    };
+
+
+    BENCHMARK_ADVANCED("Int own")(Catch::Benchmark::Chronometer meter)
+    {
+      std::string str{ "123" };
+      meter.measure([&str]() { return Converter::StringToInt(str, 0, 3); });
+    };
+  }
 }
