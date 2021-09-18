2b042611e59ba5824390ff81b859f0a8ecb8bf2b
Ian Cullen <ian.james.cullen@gmail.com>
2021-09-18 16:19:31 +0100

    Force link time option so we can compile with clang

--- cmake/ExternalLibraries.cmake
+++ cmake/ExternalLibraries.cmake
@@ -1,5 +1,8 @@
 function(add_external_libraries project_name)
   # Use the {fmt} library. It will be part of C++20
+  # clang needs the -fPIC when linking so force it
+  # GCC appears to work without it
+  set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
   add_subdirectory(external/fmt EXCLUDE_FROM_ALL)
   # Using fmt::output_file requires the compiled library. We can't use header only
   target_link_libraries(project_options INTERFACE fmt::fmt)
