766f10d49aeda4742aae164524dcab972ed18517
Ian Cullen <ian.james.cullen@gmail.com>
2022-01-01 18:05:03 +0000

    Don't force linking against stdc++fs, let the compiler take care of it
    Past me wasn't helpful about which versions the 'older' compilers
    were. As we now compile against c++20 lets hope this is no longer an
    issue.

--- cmake/ExternalLibraries.cmake
+++ cmake/ExternalLibraries.cmake
@@ -6,8 +6,4 @@ function(add_external_libraries project_name)
   add_subdirectory(external/fmt EXCLUDE_FROM_ALL)
   # Using fmt::output_file requires the compiled library. We can't use header only
   target_link_libraries(project_options INTERFACE fmt::fmt)
-
-  # Need to link the filesystem library in older compiler versions
-  # But not for MSVC
-  target_link_libraries(project_options INTERFACE $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:stdc++fs>)
 endfunction(add_external_libraries)
