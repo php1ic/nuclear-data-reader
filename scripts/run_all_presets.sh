f524e2f57ad4bc3e23b61274e0c71bd6bb2e5721
Ian Cullen <ian.james.cullen@gmail.com>
2023-06-10 15:46:22 +0100

    Update the run_all_presets script
    Add a verbosity flag to show the individual steps and their result,
    rather than the overall outcome and do some refactoring.

--- scripts/run_all_presets.sh
+++ scripts/run_all_presets.sh
@@ -4,11 +4,34 @@ scriptdir=$(readlink -f "${BASH_SOURCE%/*}")
 #shellcheck source=/dev/null
 source "${scriptdir}"/common_functions.sh
 
+VERBOSE=0
+
+# Assume that the same name is used for config, build and test
+# If it isn't this script will fail with the assumption
+PRESETS=(
+  "gcc-Debug" \
+  "gcc-Release" \
+  "clang-Debug" \
+  "clang-Release"
+)
+
+#TODO: Allow the preset(s) to be passed as arguments
+function usage {
+  echo -e "
+  ${BLUE}USAGE${RESTORE}: ${BASH_SOURCE##*/} [-v]
+
+  Optional Arguments:
+    -v  Show the result status of the individual steps (not the full output), use as ON/OFF flag [default: OFF]
+  "
+
+  exit 1
+}
+
 #Check cmake is installed
 if ! command -v cmake >/dev/null 2>&1
 then
     echo -e "\n\t${RED}ERROR${RESTORE}: The command cmake isn't installed\n"
-    exit 1
+    exit 2
 fi
 
 root_dir=$(readlink -f "${BASH_SOURCE%/*}/../")
@@ -17,33 +40,91 @@ root_dir=$(readlink -f "${BASH_SOURCE%/*}/../")
 if [[ "${PWD}" != "${root_dir}" ]]
 then
   echo -e "\t${RED}ERROR${RESTORE} : You must run this script from the root of the project (i.e. ${root_dir})"
-  exit 1
+  exit 3
 fi
 
-function run_CI {
-  cmake --preset "${1}" && \
-  cmake --build --preset "${1}" && \
-  ctest --preset "${1}"
+while getopts ":hv" OPTIONS
+do
+    case "${OPTIONS}" in
+        h | \? | : )
+            usage
+            ;;
+        v )
+            VERBOSE=1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+function run_step {
+  # 1 = preset name
+  # 2 = console string
+  # 3 = command
+  # 4 = output file
+  local ERROR_CODE
+  local MESSAGE="Running ${1} ${2}"
+
+  if [[ $VERBOSE -eq 1 ]]
+  then
+    echo -en "[ -- ] ${MESSAGE}"
+  fi
+
+  ${3} "${1}" >> "${4}" 2>&1 ; ERROR_CODE=$?
+
+  if [[ $VERBOSE -eq 1 ]]
+  then
+    if [[ ${ERROR_CODE} -eq 0 ]]
+    then
+      echo -e "\r[${GREEN}PASS${RESTORE}] ${MESSAGE}"
+    else
+      echo -e "\r[${RED}FAIL${RESTORE}] ${MESSAGE}"
+    fi
+  fi
 }
 
-presets=(
-  "gcc-Debug" \
-  "gcc-Release" \
-  "clang-Debug" \
-  "clang-Release"
-)
+function run_config {
+  run_step "${1}" "config" "cmake --preset" "${2}"
+}
 
-for p in "${presets[@]}"
-do
-  outfile="cmake_output_${p}.log"
-  run_CI "${p}" >"${outfile}" 2>&1 ; ERROR=$?
-  if [[ "${ERROR}" -eq 0 ]]
+function run_build {
+  run_step "${1}" "build" "cmake --build --preset" "${2}"
+}
+
+function run_test {
+  run_step "${1}" "tests" "ctest --preset" "${2}"
+}
+
+function run_CI {
+  run_config "${1}" "${2}" && \
+  run_build "${1}" "${2}" && \
+  run_test "${1}" "${2}"
+}
+
+function summary {
+  if [[ "${1}" -eq 0 ]]
   then
     echo -en "[${GREEN}PASS${RESTORE}] "
   else
     echo -en "[${RED}FAIL${RESTORE}] "
   fi
-  echo "${p}"
+
+  echo "${2}"
+}
+
+for p in "${PRESETS[@]}"
+do
+  outfile="cmake_output_${p}.log"
+  # Without 'true', linters complain
+  true > "${outfile}"
+
+  run_CI "${p}" "${outfile}" ; RETURNVAL=$?
+
+  if [[ ${VERBOSE} -eq 0 ]]
+  then
+    summary "${RETURNVAL}" "${p}"
+  else
+    echo " ---- "
+  fi
 done
 
 exit 0
